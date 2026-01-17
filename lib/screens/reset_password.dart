4a165b01a6ff6cc5f3e21ea835d70ce4e92e470d
xjessxx <jessica.mcilree@mymail.champlain.edu>
2026-01-17 15:27:32 -0500

    google auth and signin

---
+++ lib/screens/reset_password.dart
@@ -0,0 +1,60 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '/reusable_widgets/reusable_widget.dart';
+
+class ResetPassword extends StatefulWidget {
+  const ResetPassword({super.key});
+
+  @override
+  _ResetPasswordState createState() => _ResetPasswordState();
+}
+
+class _ResetPasswordState extends State<ResetPassword> {
+  final TextEditingController _emailTextController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Text(
+          "Reset Password",
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+            Colors.blue,
+            const Color.fromARGB(255, 46, 16, 165),
+            const Color.fromARGB(255, 12, 0, 14),
+          ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+          child: SingleChildScrollView(
+              child: Padding(
+            padding: EdgeInsets.fromLTRB(20, 120, 20, 0),
+            child: Column(
+              children: <Widget>[
+                const SizedBox(
+                  height: 20,
+                ),
+                reusableTextField("Enter Email Id", Icons.person_outline, false,
+                    _emailTextController),
+                const SizedBox(
+                  height: 20,
+                ),
+                firebaseUIButton(context, "Reset Password", () {
+                  FirebaseAuth.instance
+                      .sendPasswordResetEmail(email: _emailTextController.text)
+                      .then((value) => Navigator.of(context).pop());
+                })
+              ],
+            ),
+          ))),
+    );
+  }
+}
